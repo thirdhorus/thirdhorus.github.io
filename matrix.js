399f54334a0ff4987af3710dec51d51eca6da761
Ahmed Sghaier <benlade991@gmail.com>
2025-12-18 05:48:48 +0100

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -5,12 +5,12 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*';
-const fontSize = 16;
+const fontSize = 18;
 const columns = canvas.width / fontSize;
 const drops = Array(Math.floor(columns)).fill(1);
 
 function draw() {
-  ctx.fillStyle = 'rgba(0,0,0,0.05)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = '#00f5ff';
